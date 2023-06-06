52f46eabf1fa20a3068c8264345aff0d570830da
VivianePecenin <vivipece555@gmail.com>
2023-06-05 22:10:31 -0300

    ScreenGlicose

--- src/navigation/app-Navigator.tsx
+++ src/navigation/app-Navigator.tsx
@@ -2,6 +2,7 @@ import { NativeStackNavigationOptions, createNativeStackNavigator } from "@react
 import { AppStackParamList } from "./interface"
 import { useTheme } from "native-base"
 import HomeScreen from "../screens/Home/HomeScreen"
+import GlucosaScreen from "../screens/Glucosa/GlucosaScreen"
 
 const Stack = createNativeStackNavigator<AppStackParamList>()
 
@@ -15,9 +16,10 @@ const AppNavigator = () => {
       headerTintColor: '#ffffff'
   }
   return(
-      <Stack.Navigator screenOptions={navigatorScreenOptions}>
+      <Stack.Navigator screenOptions={navigatorScreenOptions} initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen}/>
         {/* <Stack.Screen name="Register" component={RegisterScreen}/> */}
+        {/*<Stack.Screen name='Glucose' component={GlucosaScreen}/> */}
       </Stack.Navigator>
   )
 }
--- src/navigation/interface.ts
+++ src/navigation/interface.ts
@@ -2,6 +2,7 @@ import { NativeStackNavigationProp } from "@react-navigation/native-stack"
 
 export type AppStackParamList = {
   Home: undefined,
+  Glucose: undefined,
 }
 
 export type AppNavigatorProps = NativeStackNavigationProp<AppStackParamList>
