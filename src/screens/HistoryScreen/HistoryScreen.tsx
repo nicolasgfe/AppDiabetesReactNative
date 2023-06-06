e61a0a57d7e2c384be1b6ba0c1bfc2c9db1b30af
Nicolas <nicolasgfe3@gmail.com>
2023-06-06 13:10:43 -0300

    adicionando historyScreen.

--- src/screens/HistoryScreen/HistoryScreen.tsx
+++ src/screens/HistoryScreen/HistoryScreen.tsx
@@ -1,9 +1,50 @@
 import { TextBase } from "react-native"
+import React from "react"
+import { FlatList, Text, View } from "native-base"
+import { SafeAreaView } from "react-native-safe-area-context"
+import { Item } from "../components"
 
 const HistoryScreen = () => {
-  return(
-    <TextBase>History Screen</TextBase>
-  )
+  const data = [
+    {
+      id: "1",
+      medida: 1,
+      date: "05/06/2023"
+    },
+    {
+      id: "2",
+      medida: 10,
+      date: "06/06/2023"
+    },
+    {
+      id: "3",
+      medida: 20,
+      date: "07/06/2023"
+    },
+  ]
+  
+  return (
+    <SafeAreaView >
+      <FlatList
+        data={data}
+        renderItem={({item}) => <Item medida={item.medida} date={item.date}/>}
+        keyExtractor={item => item.id}
+      />
+      
+    </SafeAreaView>
+  );
+
+  // return(
+  //   <>
+  //   <Text>Eaeee</Text>
+  //     {data.map((data) => {
+  //       <>
+  //         <Text>123</Text>
+  //         <Item data={data}/>
+  //       </>
+  //     })}
+  //   </>
+  // )
 }
 
 export {
