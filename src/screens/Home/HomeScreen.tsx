962dc2b494fb2a6f3be380c7741065fd45a91eb1
Nicolas <nicolasgfe3@gmail.com>
2023-06-18 18:34:17 -0300

    Nicolas: Ajustando commit.

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,30 +1,31 @@
 import React from "react"
-// import { Container, Title, Label, MainBox, TipsBox, LinksContainer } from "./Style";
-import { Link, Spacer } from "native-base";
+import { Container, Title, Label, MainBox, TipsBox, LinksContainer } from "./Style";
+import { Link,Text,  Spacer } from "native-base";
 
 const HomeScreen = () => {
   return(
-    <></>
-    // <Text> GLICEMIA - ÚLTIMA MEDIÇÃO </Text>
-    // <Container>
-    //   <Title> GLICEMIA - ÚLTIMA MEDIÇÃO </Title>
+    <>
+    <Text> GLICEMIA - ÚLTIMA MEDIÇÃO </Text>
+    <Container>
+      <Title> GLICEMIA - ÚLTIMA MEDIÇÃO </Title>
 
-    //   <MainBox>
+      <MainBox>
       
-    //   </MainBox>
+      </MainBox>
 
-    //   <LinksContainer>
-    //     <Link>O que é glicemia?</Link>
-    //     <Spacer />
-    //     <Link>Meu histórico de medições</Link>
-    //   </LinksContainer>
+      <LinksContainer>
+        <Link>O que é glicemia?</Link>
+        <Spacer />
+        <Link>Meu histórico de medições</Link>
+      </LinksContainer>
 
-    //   <Label>   Dicas de exercícios:</Label>
-    //   <TipsBox></TipsBox>
+      <Label>   Dicas de exercícios:</Label>
+      <TipsBox></TipsBox>
 
-    //   <Label>Dicas de nutrição:</Label>
-    //   <TipsBox></TipsBox>
-    // </Container>
+      <Label>Dicas de nutrição:</Label>
+      <TipsBox></TipsBox>
+    </Container>
+    </>
   )
 }
 
